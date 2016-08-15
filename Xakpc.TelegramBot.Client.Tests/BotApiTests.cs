89560071401eb5c3e98e26340e315be33919dd18
Pavel <xakz.pc@gmail.com>
2016-08-15 12:26:57 +0300

    Update BotApiTests.cs

--- Xakpc.TelegramBot.Client.Tests/BotApiTests.cs
+++ Xakpc.TelegramBot.Client.Tests/BotApiTests.cs
@@ -19,11 +19,11 @@ namespace Xakpc.TelegramBot.Client.Tests
 {
     public class BotApiTests
     {
-        protected int TestChatId => 101292331;
+        protected int TestChatId => TODO;
 
         protected static ITelegramBotApiClient ConstructClient()
         {            
-            return new TelegramBotApiClient("185526337:AAHl5N9nlzhqjGloJVQmjxy6IulKjclUpvc");
+            return new TelegramBotApiClient(TODO);
         }
     }
 }
